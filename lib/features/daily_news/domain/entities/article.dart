5bf91d88ccb88e193353bff6697d2925ac1b800c
pratham404 <78720951+PrathamBhavsar@users.noreply.github.com>
2025-02-10 11:19:02 +0530

    defined entities

---
+++ lib/features/daily_news/domain/entities/article.dart
@@ -0,0 +1,41 @@
+import 'package:equatable/equatable.dart';
+
+/// entitis are business objects in an app
+/// what kind of data wil the app has
+
+class ArticleEntity extends Equatable {
+  final int? id;
+  final String? author;
+  final String? title;
+  final String? description;
+  final String? url;
+  final String? urlToImage;
+  final String? publishedAt;
+  final String? content;
+
+  const ArticleEntity({
+    this.id,
+    this.author,
+    this.title,
+    this.description,
+    this.url,
+    this.urlToImage,
+    this.publishedAt,
+    this.content,
+  });
+
+  /// which objects should be compared (equatable)
+  @override
+  List<Object?> get props {
+    return [
+      id,
+      author,
+      title,
+      description,
+      url,
+      urlToImage,
+      publishedAt,
+      content,
+    ];
+  }
+}
