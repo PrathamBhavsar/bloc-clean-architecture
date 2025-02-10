db83ade460bd2d91d6bed5d69836606c5f34648e
pratham404 <78720951+PrathamBhavsar@users.noreply.github.com>
2025-02-10 14:08:13 +0530

    added dependency injections

---
+++ lib/injection_container.dart
@@ -0,0 +1,23 @@
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+import 'package:news_app/features/daily_news/data/data_sources/remote/news_api_service.dart';
+import 'package:news_app/features/daily_news/data/repository/article_repository_impl.dart';
+import 'package:news_app/features/daily_news/domain/repository/article_repository.dart';
+import 'package:news_app/features/daily_news/domain/usecases/get_article.dart';
+import 'package:news_app/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
+
+final sl = GetIt.instance;
+
+Future<void> initializeDependencies() async {
+  sl.registerSingleton(Dio);
+
+  sl.registerSingleton<NewsApiService>(NewsApiService(sl()));
+
+  sl.registerSingleton<ArticleRepository>(ArticleRepositoryImpl(sl()));
+
+  ///use cases
+  sl.registerSingleton<GetArticleUseCase>(GetArticleUseCase(sl()));
+
+  ///bloc
+  sl.registerFactory<RemoteArticlesBloc>(() => RemoteArticlesBloc(sl()));
+}
